b631f6c5ffecf87b5eaf33d43f7a76d3e788fe7f
v-shaft <156771974+v-shaft@users.noreply.github.com>
2024-02-19 07:38:36 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,134 +1,134 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-const axios = require('axios');
 
+// Task 6: registering a new user
+public_users.post("/register", (req, res) => {
+  // Get credentials
+  const username = req.body.username;
+  const password = req.body.password;
 
-public_users.post("/register", (req,res) => {
-  if(isValid(req.body.username)){
-    return res.status(400).json({message: "Username already taken! Choose another one"});
+  // Check if username and password exist
+  if (username.length > 0 && password.length > 0) {
+    // Check if user is already registered
+    if (isValid(username)) {
+      users.push({ username: username, password: password });
+      return res
+        .status(200)
+        .json({ message: "Registration successful" });
+    } else {
+      return res.status(406).json({ message: "Error... User already exists" });
+    }
   }
-  else
-  {
-    let user = {
-      username: req.body.username,
-      password: req.body.password
-    }
-    users.push(user);
-    return res.status(200).json({message: "Registration successful"});
-  }
+  return res.status(406).json({ message: "Error... missing username or password" });
 });
 
-// task 1 - Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books))
+// Task 1: Get list of books available in the shop
+public_users.get("/", function (req, res) {
+  res.send(JSON.stringify({ books }, null, 4));
 });
 
-// task 2 - Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn=req.params.isbn;
-  res.send(books[isbn])
-  
- });
-  
-// task 3 - Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-
-  let author = req.params.author;
-  let arr = Object.entries(books)
-  const book_author = new Promise((resolve, reject)=>{
-
-    let book_by_author = arr.filter((item)=>item[1].author === author)
-    if(book_by_author)
-    {
-      resolve(book_by_author)
-    }
-    else{
-      reject({message: `Author not found: ${author}`})
-    }
-  })
-
-  book_author.then((resp)=>{
-    res.status(200).json(resp)
-  }).catch(err=>res.status(403).json({error: err}))
-  
-});
-
-// task 4 - Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  let title = req.params.title;
-  let arr = Object.entries(books)
-
-  const book_title = new Promise((resolve,reject)=>{
-    let book_by_title = arr.filter((item)=>item[1].title === title)
-    if(book_by_title)
-    {
-      resolve(book_by_title[0][1])
-    }
-    else
-    {
-      reject({message: `Book by title not found: ${title}` })
-    }
+// Task 10: Promise callbacks or async-await with Axios
+public_users.get("/async", function (req, res) {
+  const getBooks = new Promise(() => {
+    res.send(JSON.stringify({ books }));
   });
 
-  book_title.then((resp)=>{
-    res.status(200).json(resp)
-    }).catch(err=>res.status(403).json({error: err}))
+  getBooks.then(() => console.log("Task 10: Promise callbacks or async-await with Axios successful"))
 });
 
-//  task 5 - Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  const isbn=req.params.isbn;
-  res.send(JSON.stringify(books[isbn].reviews))
+// Task 2: Get book details based on ISBN
+public_users.get("/isbn/:isbn", function (req, res) {
+  const bookISBN = req.params.isbn;
+  res.send(books[bookISBN]);
 });
 
-// task 10
-let url="https://teokos1989-5000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
-const getBookDetails=async(url)=>{
-      let resp = await axios.get(url);
-      let books = resp.data;
-      books.map((book)=>{
-        console.log(book[isbn]);
-        console.log(book[isbn].author);
-        console.log(book[isbn].title);
-        console.log(book[isbn].reviews);
-      });
-}
+// Task 11: Promise callbacks or async-await with Axios
+public_users.get("/async/isbn/:isbn", function (req, res) {
+  const getBook = new Promise(() => {
+    const bookISBN = req.params.isbn;
+    res.send(books[bookISBN]);
+  });
 
-// task 11
-url="https://teokos1989-5000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/isbn/";
-const getBookDetailsByISBN=async(url,isbn)=>{
-      let resp = await axios.get(url+isbn);
-      let book = resp.data;
-      console.log(book[isbn]);
-}
+  getBook.then(() => console.log("Task 11: Promise callbacks or async-await with Axios successful"))
+});
 
-//task 12
-url="https://teokos1989-5000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/author/";
-const getBookDetailsByAuthor=async(url,author)=>{
-      let resp = await axios.get(url+author);
-      let books = resp.data;
-      books.map((book)=>{
-        console.log(book[isbn]);
-        console.log(book[isbn].author);
-        console.log(book[isbn].title);
-        console.log(book[isbn].reviews);
-      });
-}
+// Task 3: Get book details based on author
+public_users.get("/author/:author", function (req, res) {
+  const author = req.params.author;
+  const allBooksByAuthor = Object.entries(books);
+  const finalBooks = [];
 
-// task 13
-url="https://teokos1989-5000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/title/";
-const getBookDetailsByTitle=async(url,title)=>{
-      let resp = await axios.get(url+title);
-      let books = resp.data;  
-      books.map((book)=>{
-        console.log(book[isbn]);
-        console.log(book[isbn].author);
-        console.log(book[isbn].title);
-        console.log(book[isbn].reviews);
-      });
-}
+  // Find which values match the author
+  for (const [key, value] of allBooksByAuthor) {
+    if (value.author === author) {
+      finalBooks.push(value);
+    }
+  }
+  res.send(finalBooks);
+});
+
+// Task 12: Promise callbacks or async-await with Axios
+public_users.get("/async/author/:author", function (req, res) {
+  const getAuthorsBooks = new Promise(() => {
+    const author = req.params.author;
+    const allBooksByAuthor = Object.entries(books);
+    const finalBooks = [];
+  
+    // Find which values match the author
+    for (const [key, value] of allBooksByAuthor) {
+      if (value.author === author) {
+        finalBooks.push(value);
+      }
+    }
+    res.send(finalBooks);
+  })
+
+  getAuthorsBooks.then(() => console.log("Task 12: Promise callbacks or async-await with Axios successful"))
+});
+
+// Task 4: Get all books based on title
+public_users.get("/title/:title", function (req, res) {
+  const title = req.params.title;
+  const allBooksByTitle = Object.entries(books);
+  const finalBooks = [];
+
+  // Find which values match the title
+  for (const [key, value] of allBooksByTitle) {
+    if (value.title === title) {
+      finalBooks.push(value);
+    }
+  }
+  res.send(finalBooks);
+});
+
+// Task 13: Promise callbacks or async-await with Axios
+public_users.get("/async/title/:title", function (req, res) {
+  const getBookTitles = new Promise(() => {
+    const title = req.params.title;
+    const allBooksByTitle = Object.entries(books);
+    const finalBooks = [];
+  
+    // Find which values match the title
+    for (const [key, value] of allBooksByTitle) {
+      if (value.title === title) {
+        finalBooks.push(value);
+      }
+    }
+    res.send(finalBooks);
+  });
+
+  getBookTitles.then(() => console.log("Task 13: Promise callbacks or async-await with Axios successful"))
+});
+
+// Task 5: Get book review
+public_users.get("/review/:isbn", function (req, res) {
+  const bookISBN = req.params.isbn;
+  const book = books[bookISBN];
+
+  res.send(book.reviews);
+});
 
 module.exports.general = public_users;
